b0add9a6d9aafa0011b859622401b6ee93690be1
Robert Saavedra Jr <robscafe333@yahoo.com>
2020-02-12 18:41:10 -0800

    All

---
+++ Practice/PTipCalculator.js
@@ -0,0 +1,10 @@
+var billAmt = document.getElementById("billAmt").value;
+var serviceQual = document.getElementById('serviceQual').value;
+var numPeople = document.getElementById('numPeople').value;
+
+if (billAmt === "" || serviceQual =="0") {
+  alert('Please enter value');
+  return;
+}else{
+  
+}
