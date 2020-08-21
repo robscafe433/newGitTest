1613e3ce8ae850b70c8d6d3ba195f77ec07ff6fc
Robert Saavedra Jr <robscafe333@yahoo.com>
2020-08-21 13:33:32 -0700

    added notes on bug I encountered

--- LOLCat/LOLcatJS08-21-20.js
+++ LOLCat/LOLcatJS08-21-20.js
@@ -1,3 +1,5 @@
+// I was practicing eventListeners. A bug that I could not figure out was line 16, where the image was not changing, so that is reason i added the alerts to actually see if the if statements were working. Originally I wrote line 16 as "lolcatImageJS.innerText =" and "lolcatImageJS.innerHTML =" but eventually figured I had to use the ".src" as images are not treated the same as texts. //
+
 var wakeUpTimeSelectorJS = document.getElementById("wakeUpTimeSelector");
 var lolcatImageJS = document.getElementById("lolcatImage");
 var messageJS = document.getElementById("messageText");
@@ -9,7 +11,7 @@ function addTimeChosen() {
 
   if (timeChosen == 4) {
     alert(timeChosen);
-    
+
     lolcatImageJS.src =
       "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat1.jpg";
     messageJS.innerText = "Wake-Up!!!!!!!";
