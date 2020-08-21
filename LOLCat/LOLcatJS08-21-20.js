c7df9421a39c6b5fb8ace9d9e569ffe95d5f271d
Robert Saavedra Jr <robscafe333@yahoo.com>
2020-08-21 13:36:06 -0700

    added notes on bug

--- LOLCat/LOLcatJS08-21-20.js
+++ LOLCat/LOLcatJS08-21-20.js
@@ -1,4 +1,8 @@
-// I was practicing eventListeners. A bug that I could not figure out was line 16, where the image was not changing, so that is reason i added the alerts to actually see if the if statements were working. Originally I wrote line 16 as "lolcatImageJS.innerText =" and "lolcatImageJS.innerHTML =" but eventually figured I had to use the ".src" as images are not treated the same as texts. //
+// I was practicing eventListeners. A bug that I could not figure out was line
+// 16, where the image was not changing, so that is reason i added the alerts to
+// actually see if the if statements were working. Originally I wrote line 16 as
+//"lolcatImageJS.innerText =" and "lolcatImageJS.innerHTML =" but eventually
+//figured I had to use the ".src" as images are not treated the same as texts.
 
 var wakeUpTimeSelectorJS = document.getElementById("wakeUpTimeSelector");
 var lolcatImageJS = document.getElementById("lolcatImage");
