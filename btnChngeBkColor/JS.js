b00f2d4c3083057c1735fe5ceb6dd89a07158ce8
Robert Saavedra Jr <robscafe333@yahoo.com>
2020-08-21 22:28:46 -0700

    changed code in html so onclick calls the function

--- btnChngeBkColor/JS.js
+++ btnChngeBkColor/JS.js
@@ -1,7 +1,12 @@
-var blueJS = document.getElementsByClassName("blue");
-
 var changeBkBlue = function () {
   document.body.style.background = "blue";
 };
-
-blueJS.addEventListener("click", changeBkBlue);
+var changeBkRed = function () {
+  document.body.style.background = "red";
+};
+var changeBkYellow = function () {
+  document.body.style.background = "yellow";
+};
+var changeBkGreen = function () {
+  document.body.style.background = "green";
+};
