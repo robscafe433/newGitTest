c10fcffea2e817e2e8b212eefa9fa728ad478098
Robert Saavedra Jr <robscafe333@yahoo.com>
2020-08-21 19:45:29 -0700

    trying to create buttons that change background but not working

---
+++ btnChngeBkColor/JS.js
@@ -0,0 +1,7 @@
+var blueJS = document.getElementsByClassName("blue");
+
+var changeBkBlue = function () {
+  document.body.style.background = "blue";
+};
+
+blueJS.addEventListener("click", changeBkBlue);
